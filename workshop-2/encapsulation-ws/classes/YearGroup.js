b19bbd27a4a4fa0ad1cbcb5781e3f13142348542
KaltunAhmed <Kaltahmed94@gmail.com>
2019-03-12 12:12:02 +0000

    workshop stuff

---
+++ workshop-2/encapsulation-ws/classes/YearGroup.js
@@ -0,0 +1,18 @@
+class YearGroup {
+    constructor(year,headOfYear){
+        this.year = year
+        this.headOfYear = headOfYear
+    }
+    YearGroup(int,string){
+
+    }
+    getYear(year){
+        return this.year
+    }
+    getHeadOfYear(headOfYear){
+        return this.headOfYear
+    }
+
+}
+
+module.exports = YearGroup
